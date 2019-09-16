1c6b4d8cde11710f48464ff43465f24e1159c89b
Roy Vashisht <rishavvashisht845@gmail.com>
2019-09-16 19:13:31 -0400

    Add project files.

---
+++ comp2084 lab-1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using comp2084_lab_1.Models;
+
+namespace comp2084_lab_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult addition(int a, int b)
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return Ok("result ="+(a+b).ToString());
+        }
+
+        public IActionResult multiply(int a, int b)
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return Ok("result =" + (a * b).ToString());
+        }
+    }
+}
